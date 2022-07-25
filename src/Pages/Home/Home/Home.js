e13ba18a5cd787607f5888cc2f353677c18f1ca5
orun-sunny <sunny.mukut@gmail.com>
2022-07-26 00:41:05 +0600

    2nd xommitt

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -5,12 +5,14 @@ const Home = () => {
     return (
         <div>
            <header className='bg-black'>
-            <h1 className='text-white font-bold'>Hello, It's me</h1>
+            <h1 className='text-white font-bold text-xl'>Hello, It's me</h1>
             <br />
-           <div className='flex-1 '>
-           <h6 className='text-white font-extrabold'>Ojieame</h6> 
-            <span className='dot vf font-extrabold'>.</span>
+           <div className='flex '>
+           <h6 className='text-white font-extrabold text-6xl'>Ojieame</h6> 
+            <span className='dot vf font-extrabold text-6xl'>.</span>
            </div>
+           <br />
+
            <span>
            <p className='text-white '>
            An enthusiastic product designer currently shaping the future of software development by designing smooth user-interfaces that promote user interaction with information and data. While traveling around the world. 
